798d46449542cee4581119015dfe673292346959
Marek Hauschwitz <hauschwitz@wpj.cz>
2024-12-22 19:59:56 +0100

    04b

--- src/04.cpp
+++ src/04.cpp
@@ -1,5 +1,8 @@
+#include "./utils.h"
+
 #include <cassert>
 #include <fstream>
+#include <ios>
 #include <iostream>
 #include <sstream>
 #include <string>
@@ -64,30 +67,8 @@ bool reverse_equals(const std::string_view& one, const std::string_view& second)
     return true;
 }
 
-// Moc hezký to není :)
-int main(int argc, char* argv[])
+int solve_and_print_a(const Board& board)
 {
-    (void)argc;
-    (void)argv;
-#ifdef TEST
-    std::string input{ "MMMSXXMASM\n"
-                       "MSAMXMSMSA\n"
-                       "AMXSXMAAMM\n"
-                       "MSAMASMSMX\n"
-                       "XMASAMXAMM\n"
-                       "XXAMMXXAMA\n"
-                       "SMSMSASXSS\n"
-                       "SAXAMASAAA\n"
-                       "MAMMMXMMMM\n"
-                       "MXMXAXMASX\n" };
-#else
-    std::ifstream input_stream{ "./input/04.txt" };
-    std::ostringstream oss;
-    oss << input_stream.rdbuf();
-    std::string input = oss.str();
-#endif
-
-    Board board{ input };
     const std::string_view needle = "XMAS";
 
     const size_t max_col = board.n_cols - needle.length();
@@ -318,6 +299,171 @@ int main(int argc, char* argv[])
     }
 
     std::cout << '\n';
-
     std::cout << "Count: " << count << '\n';
+
+    return 0;
+}
+
+struct Position
+{
+    size_t row, col;
+};
+
+struct MasOccurrence
+{
+    std::array<Position, 3> positions{};
+
+    bool intersects(const MasOccurrence& other) const
+    {
+        return other.positions[1].col == positions[1].col &&
+               other.positions[1].row == positions[1].row;
+    }
+};
+
+// Bylo by jednodušší a rychleší ukládat jen prostřední char
+// do nějaký mapy a pak vybrat jen klíče, které > 1.
+// Ale takhle to je jednodušší debuggovat, kdybych to někde
+// napsal špatně.
+int solve_and_print_b(const Board& board)
+{
+    const std::string needle = "MAS";
+    std::vector<MasOccurrence> occurrences;
+
+    const size_t max_row = board.n_rows - needle.length();
+    const size_t max_col = board.n_cols - needle.length();
+
+    // \ Diagonally
+
+    for (size_t row = 0; row <= max_row; row++) {
+        for (size_t col = 0; col <= max_col; col++) {
+            MasOccurrence occ;
+
+            bool matches_forwards = true;
+            for (size_t offset = 0; offset < needle.length(); offset++) {
+                if (board.at(row + offset, col + offset) != needle[offset]) {
+                    matches_forwards = false;
+                }
+
+                occ.positions[offset] = { .row = row + offset,
+                                          .col = col + offset };
+            }
+
+            if (matches_forwards) {
+                occurrences.push_back(std::move(occ));
+
+                continue;
+            }
+
+            bool matches_backwards = true;
+            for (size_t offset = 0; offset < needle.length(); offset++) {
+                if (board.at(row + offset, col + offset) !=
+                    needle[needle.length() - offset - 1]) {
+                std::internal(ios_base &base)
+                    matches_backwards = false;
+                }
+
+                occ.positions[offset] = { .row = row + offset,
+                                          .col = col + offset };
+            }
+
+            if (matches_backwards) {
+                occurrences.push_back(std::move(occ));
+            }
+        }
+    }
+
+    // / Diagonally
+    for (size_t row = 0; row <= max_row; row++) {
+        for (size_t col = board.n_cols - 1; col >= needle.length() - 1; col--) {
+            MasOccurrence occ;
+
+            bool matches_forwards = true;
+            for (size_t offset = 0; offset < needle.length(); offset++) {
+                if (board.at(row + offset, col - offset) != needle[offset]) {
+                    matches_forwards = false;
+                }
+
+                occ.positions[offset] = { .row = row + offset,
+                                          .col = col - offset };
+            }
+
+            if (matches_forwards) {
+                occurrences.push_back(std::move(occ));
+
+                continue;
+            }
+
+            bool matches_backwards = true;
+            for (size_t offset = 0; offset < needle.length(); offset++) {
+                if (board.at(row + offset, col - offset) !=
+                    needle[needle.length() - offset - 1]) {
+                    matches_backwards = false;
+                }
+
+                occ.positions[offset] = { .row = row + offset,
+                                          .col = col - offset };
+            }
+
+            if (matches_backwards) {
+                occurrences.push_back(std::move(occ));
+            }
+        }
+    }
+
+    for (const auto& occ : occurrences) {
+        std::cout << std::format("Occurence ({}, {}), ({}, {}), ({}, {})\n",
+                                 occ.positions[0].row,
+                                 occ.positions[0].col,
+                                 occ.positions[1].row,
+                                 occ.positions[1].col,
+                                 occ.positions[2].row,
+                                 occ.positions[2].col);
+    }
+
+    int intersections_count = 0;
+
+    for (size_t i = 0; i < occurrences.size(); i++) {
+        const MasOccurrence& ith_occ = occurrences[i];
+
+        for (size_t j = i + 1; j < occurrences.size(); j++) {
+            if (ith_occ.intersects(occurrences[j])) {
+                intersections_count++;
+            }
+        }
+    }
+
+    std::cout << "Intersections count: " << intersections_count << '\n';
+
+    return 0;
+}
+
+int main(int argc, char* argv[])
+{
+    (void)argc;
+    (void)argv;
+#ifdef TEST
+    std::string input{ "MMMSXXMASM\n"
+                       "MSAMXMSMSA\n"
+                       "AMXSXMAAMM\n"
+                       "MSAMASMSMX\n"
+                       "XMASAMXAMM\n"
+                       "XXAMMXXAMA\n"
+                       "SMSMSASXSS\n"
+                       "SAXAMASAAA\n"
+                       "MAMMMXMMMM\n"
+                       "MXMXAXMASX\n" };
+#else
+    std::ifstream input_stream{ "./input/04.txt" };
+    std::ostringstream oss;
+    oss << input_stream.rdbuf();
+    std::string input = oss.str();
+#endif
+
+    Board board{ input };
+
+    if (!is_second_solution(argc, argv)) {
+        return solve_and_print_a(board);
+    } else {
+        return solve_and_print_b(board);
+    }
 }
