f4f3d2cf2b89c46035f39b9d8846d75f1c4397e8
Marek Hauschwitz <hauschwitz@wpj.cz>
2024-12-25 15:55:22 +0100

    fix

--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-[[ "${COMPILER}" != ""]] || COMPILER="clang++"
+[[ "${COMPILER}" != "" ]] || COMPILER="clang++"
+
 FLAGS="-o2 -std=c++23 -Wall -Wextra -Wpedantic -Werror -Wfloat-equal -fdiagnostics-color=always -Wno-sign-compare ${FLAGS}"
 
 if [[ "${BIN_PATH}" == "" ]]; then
